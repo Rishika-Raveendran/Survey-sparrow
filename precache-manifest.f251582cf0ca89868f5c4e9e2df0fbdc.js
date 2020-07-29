c47775f5fa5efa1279b0b6de23c4aea173af5dfa
Rishika <rishikarocky14@gmail.com>
2020-07-29 14:38:43 +0530

    Updates

---
+++ precache-manifest.f251582cf0ca89868f5c4e9e2df0fbdc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f80bfc9abc3b280f50a776595c11030f",
+    "url": "/Web-design/index.html"
+  },
+  {
+    "revision": "e3933efda66ac6069221",
+    "url": "/Web-design/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2468d2f6c0d5d07b0fa4",
+    "url": "/Web-design/static/css/main.0c58725c.chunk.css"
+  },
+  {
+    "revision": "e3933efda66ac6069221",
+    "url": "/Web-design/static/js/2.669007f2.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Web-design/static/js/2.669007f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2468d2f6c0d5d07b0fa4",
+    "url": "/Web-design/static/js/main.e6c94534.chunk.js"
+  },
+  {
+    "revision": "4f1a83b1e87151bf1489",
+    "url": "/Web-design/static/js/runtime-main.e246e9da.js"
+  },
+  {
+    "revision": "4a6ebd5ac6c39df826439199c960c8ef",
+    "url": "/Web-design/static/media/Banner.4a6ebd5a.png"
+  }
+]);
